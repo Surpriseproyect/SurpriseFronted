554cd65f6e3b5065e71f48e774039709427b9f13
JhoanMonsalve <jhoanmonsalve320@gmail.com>
2024-07-10 10:40:52 -0500

    11

--- src/public/js/generarPDF.js
+++ src/public/js/generarPDF.js
@@ -1,29 +1,25 @@
-// Crea un archivo JavaScript (por ejemplo, main.js) y agrégalo a tu página HTML usando un elemento <script type="module">
-// main.js
-
-
 document.addEventListener('DOMContentLoaded', async () => {
-    const pdfDoc = await window.PDFLib.PDFDocument.create();
     const botonesGenerarPDF = document.querySelectorAll('.generar');
 
-  botonesGenerarPDF.forEach(boton => {
-    boton.addEventListener('click', async () => {
-      const facturaContainer = boton.closest('.container');
-      const facturaID = facturaContainer.querySelector('.id').textContent;
+    botonesGenerarPDF.forEach(boton => {
+        boton.addEventListener('click', async () => {
+            const facturaContainer = boton.closest('.container');
+            const facturaID = facturaContainer.querySelector('.id').textContent;
 
-      const pdfDoc = await PDFDocument.create();
-      const page = pdfDoc.addPage();
+            // Crea un nuevo documento PDF
+            const pdfDoc = await window.PDFLib.PDFDocument.create();
+            const page = pdfDoc.addPage();
 
-      // Agrega contenido al PDF
-      page.drawText(`Factura ID: ${facturaID}`, { x: 50, y: 700 });
+            // Agrega contenido al PDF
+            page.drawText(`Factura ID: ${facturaID}`, { x: 50, y: 700 });
 
-      // Guarda el PDF y descárgalo
-      const pdfBytes = await pdfDoc.save();
-      const blob = new Blob([pdfBytes], { type: 'application/pdf' });
-      const link = document.createElement('a');
-      link.href = window.URL.createObjectURL(blob);
-      link.download = `factura_${facturaID}.pdf`;
-      link.click();
+            // Guarda el PDF y descárgalo
+            const pdfBytes = await pdfDoc.save();
+            const blob = new Blob([pdfBytes], { type: 'application/pdf' });
+            const link = document.createElement('a');
+            link.href = window.URL.createObjectURL(blob);
+            link.download = `factura_${facturaID}.pdf`;
+            link.click();
+        });
     });
-  });
 });
